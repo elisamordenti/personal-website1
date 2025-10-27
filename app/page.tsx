26a9ed721d976566c88486579b91cf536d89737e
elisamordenti <elisamordenti1@gmail.com>
2025-10-27 22:38:40 +0100

    Update background to light blue-grey

--- app/page.tsx
+++ app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
   }, []); // Empty dependency array - no more infinite loop!
 
   return (
-    <div className="min-h-screen relative" style={{ background: '#e6f3ff' }}>
+    <div className="min-h-screen relative" style={{ background: '#e8f2f8' }}>
       {/* Particle Field - Centered */}
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="relative">
